1b090fb7c3cfd6923f0b10ffc979c09127439955
Vallabh Vidyasagar <vallabhvidyasagar10@gmail.com>
2025-06-25 17:27:20 +0530

    add basic fab support

---
+++ lib/screens/home_screen/widgets/home_fab.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
+
+class HomeFAB extends StatelessWidget {
+  const HomeFAB({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpandableFab(children: []);
+  }
+}
